ffffc2cfdd3ef292b44ec2edb958421b4efca235
jasonrfeliz <jason@meetmycampus.com>
2019-02-25 21:18:13 -0500

    migrated favorites table

--- db/migrate/20190226020955_create_favorite_trails.rb
+++ db/migrate/20190226020955_create_favorite_trails.rb
@@ -1,8 +1,8 @@
 class CreateFavoriteTrails < ActiveRecord::Migration[5.2]
   def change
     create_table :favorite_trails do |t|
-      t.reference :user
-      t.int :trail_id
+      t.references :user
+      t.integer :trail_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_23_191514) do
+ActiveRecord::Schema.define(version: 2019_02_26_020955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorite_trails", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "trail_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorite_trails_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
