4d5af75d0c2c7ec3bd3181eb91568ed0b5719938
jasonrfeliz <jason@meetmycampus.com>
2019-02-26 06:53:24 -0500

    some configurations

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative 'boot'
 
-require "rails"
+# require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
@@ -8,7 +8,7 @@ require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "action_view/railtie"
+# require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
@@ -20,7 +20,7 @@ Bundler.require(*Rails.groups)
 module LetsHike
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    # config.load_defaults 5.2
     config.api_only = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
